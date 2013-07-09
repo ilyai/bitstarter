ff84a38d05e98400d3e23d7a658b1b30825b4091
Ilya Igonkin <osdev0@gmail.com>
2013-07-10 00:24:08 +0600

    Add checks and url support for grader

--- grader.js
+++ grader.js
@@ -24,6 +24,7 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var rest = require('restler');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
 
@@ -36,16 +37,25 @@ var assertFileExists = function(infile) {
     return instr;
 };
 
-var cheerioHtmlFile = function(htmlfile) {
-    return cheerio.load(fs.readFileSync(htmlfile));
+var assertValidUrl = function(url) {
+  url = url.toString();
+  if (url.indexOf('http') !== 0) {
+    console.error("%s is not a valid URL", url);
+    process.exit(1);
+  }
+  return url;
+};
+
+var cheerioLoadHtml = function(html) {
+    return cheerio.load(html);
 };
 
 var loadChecks = function(checksfile) {
     return JSON.parse(fs.readFileSync(checksfile));
 };
 
-var checkHtmlFile = function(htmlfile, checksfile) {
-    $ = cheerioHtmlFile(htmlfile);
+var checkHtml = function(html, checksfile) {
+    $ = cheerioLoadHtml(html);
     var checks = loadChecks(checksfile).sort();
     var out = {};
     for(var ii in checks) {
@@ -53,6 +63,12 @@ var checkHtmlFile = function(htmlfile, checksfile) {
         out[checks[ii]] = present;
     }
     return out;
+};
+
+var grade = function(html, checksfile) {
+  var checkJson = checkHtml(html, checksfile);
+  var outJson = JSON.stringify(checkJson, null, 4);
+  console.log(outJson);
 };
 
 var clone = function(fn) {
@@ -64,11 +80,20 @@ var clone = function(fn) {
 if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
-        .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), '')
+        .option('-f, --url <html_url>', 'Url to index.html', clone(assertValidUrl), '')
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+    if (program.file) {
+      grade(fs.readFileSync(program.file).toString(), program.checks);
+    } else if (program.url) {
+      rest.get(program.url).on('complete', function(result) {
+        if (result instanceof Error) throw result;
+        grade(result.toString(), program.checks);
+      });
+    } else {
+      console.error("Either file or url must be specified");
+      process.exit(1);
+    }
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
